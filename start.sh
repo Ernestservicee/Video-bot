bdcd1aabb864e6c42b1d095d1568f2dff36703e9
Ernestservicee <110431739+Ernestservicee@users.noreply.github.com>
2024-11-10 19:19:35 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
-cd /VCPlayerBot
+git clone https://github.com/Ernestservicee/Video-bot /Video-bot
+cd /Video-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
